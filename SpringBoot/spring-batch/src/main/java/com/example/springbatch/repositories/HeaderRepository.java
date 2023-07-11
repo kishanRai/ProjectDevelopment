a33ae8df94ded688b9e5c668aacb992ff1a72aac
kishan <erkishanrai@gmail.com>
2023-07-11 08:49:29 +0530

    Spring Batch HeaderRepository Modifications
    Signed-off-by: kishan <erkishanrai@gmail.com>

--- SpringBoot/spring-batch/src/main/java/com/example/springbatch/repositories/HeaderRepository.java
+++ SpringBoot/spring-batch/src/main/java/com/example/springbatch/repositories/HeaderRepository.java
@@ -4,6 +4,8 @@ import com.example.springbatch.entities.HeaderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface HeaderRepository extends JpaRepository<HeaderEntity, String> {
+public interface HeaderRepository extends JpaRepository<HeaderEntity, UUID> {
 }
